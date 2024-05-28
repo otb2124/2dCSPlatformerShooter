959b7c2cd8452414f1eed0c05a50f12a177fa532
otb2124 <158044471+otb2124@users.noreply.github.com>
2024-05-28 23:28:11 +0200

    Add files via upload

---
+++ Physics/FlatAABB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Platformer.Physics
+{
+    public readonly struct FlatAABB
+    {
+        public readonly FlatVector Min;
+        public readonly FlatVector Max;
+
+        public FlatAABB(FlatVector min, FlatVector max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public FlatAABB(float minX, float minY, float maxX, float maxY)
+        {
+            Min = new FlatVector(minX, minY);
+            Max = new FlatVector(maxX, maxY);
+        }
+    }
+}
---
+++ Physics/FlatTransform.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Platformer.Physics
+{
+    public struct FlatTransform
+    {
+        public readonly static FlatTransform Zero = new FlatTransform(0f, 0f, 0f);
+
+        public readonly float PositionX;
+        public readonly float PositionY;
+        public readonly float Sin;
+        public readonly float Cos;
+
+
+        public float CosScaleX => Cos;
+        public float SinScaleY => -Sin;
+        public float SinScaleX => Sin;
+        public float CosScaleY => Cos;
+
+        public FlatTransform(FlatVector position, float angle)
+        {
+            PositionX = position.X;
+            PositionY = position.Y;
+            Sin = MathF.Sin(angle);
+            Cos = MathF.Cos(angle);
+        }
+
+        public FlatTransform(float x, float y, float angle)
+        {
+            PositionX = x;
+            PositionY = y;
+            Sin = MathF.Sin(angle);
+            Cos = MathF.Cos(angle);
+        }
+
+
+    }
+}
